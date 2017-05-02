c9c1d8eb09952d64f00c244f6e4fc433f943f433
kevin daus <kevinflorenzdaus@gmail.com>
2017-05-02 20:50:28 +0800

    send mobile number as raw

--- protected/controllers/DispoController.php
+++ protected/controllers/DispoController.php
@@ -63,9 +63,11 @@ class DispoController extends Controller
             $_POST['phone_number'] = preg_replace("/[^0-9,.]/", "", $_POST['phone_number']);
             //if starts with 44 
             
-            // if (substr($_POST['phone_number'], 0, 2) === '44') {
-            //     $_POST['phone_number'] = '0' .  substr($_POST['phone_number'], 2, strlen($_POST['phone_number']) -1 );
-            // } else {
+            if (substr($_POST['phone_number'], 0, 2) === '44') {
+                // $_POST['phone_number'] = '0' .  substr($_POST['phone_number'], 2, strlen($_POST['phone_number']) -1 );
+                $_POST['phone_number'] = substr($_POST['phone_number'], 2, strlen($_POST['phone_number']) -1 );
+            } 
+            // else {
             //     $_POST['phone_number'] = '0' .  $_POST['phone_number'];
             // }
             
